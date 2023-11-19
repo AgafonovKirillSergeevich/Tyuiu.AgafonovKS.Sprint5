32fde2c8fe6a0e4c9609eaecdeee5986aa65b7dc
Kirya <Kirya@DESKTOP-EM62GFK>
2023-11-19 13:55:01 +0500

    Add project files.

---
+++ Tyuiu.AgafonovKS.Sprint5.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.AgafonovKS.Sprint5.Task0.V18.Lib;
+
+
+namespace Tyuiu.AgafonovKS.Sprint5.Task0.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Kirya\source\repos\Tyuiu.AgafonovKS.Sprint5\Tyuiu.AgafonovKS.Sprint5.Task0.V18\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
