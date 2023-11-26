d61b11adb873bd3068749314dea7383b7992a6d0
Кирилл Агафонов <Kirya_agafonov_2002@mail.ru>
2023-11-26 17:54:42 +0500

    Task7

---
+++ Tyuiu.AgafonovKS.Sprint5.Task7.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AgafonovKS.Sprint5.Task7.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AgafonovKS.Sprint5.Task7.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("feed98b5-3cfc-47bc-a14a-aa04446993a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.AgafonovKS.Sprint5.Task7.V4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.AgafonovKS.Sprint5.Task7.V4.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint5.Task7.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Агафонов К. С. | ИИПб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                      *");
+            Console.WriteLine("* Задание #7                                                                   *");
+            Console.WriteLine("* Вариант #4                                                                   *");
+            Console.WriteLine("* Выполнил: Агафонов Кирилл Сергеевич | ИИПб-23-1                              *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask6V18.txt (файл взять из архива        *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С: DataSprint5 и            *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор символьных данных.              *");
+            Console.WriteLine("* Заменить все русские буквы на #.                                             *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask7V4.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFile7.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Данные находятся в файле: ");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine(pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
